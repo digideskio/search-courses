0007d8d2ee227ca65b439b648cfb44ed1e0b7b15
Denis Aoki <dnsaoki@gmail.com>
2016-11-23 16:44:06 -0200

    .

--- search/dbase.py
+++ search/dbase.py
@@ -3,12 +3,12 @@ from search.models import CoursesModel
 
 class DataBase():
     def save(self, name, address):
-        if not CoursesModel.objects.filter(key=str(name)).exists():
-            objects = CoursesModel.objects.create(
-                key = str(name),
-                address = str(address)
+        if not CoursesModel.objects.filter(name=str(name)).exists():
+            object = CoursesModel.objects.create(
+                name=str(name),
+                address=str(address)
             )
-            objects.save()
+            object.save()
 
     def get(self, key):
         if not CoursesModel.objects.filter(key=str(key)).exists():
--- search/tasks.py
+++ search/tasks.py
@@ -4,21 +4,28 @@ from background_task import background
 from search.models import CoursesModel
 from search import settings
 
-@background(schedule=60*5)
 def TaskSearchCourses():
+    import ipdb
+    ipdb.set_trace()
+    print "aaaaa"
     start = 1
     key = settings.KEY
     cx = settings.CX
     url = "https://www.googleapis.com/customsearch/v1"
     q = "key={}&cx={}".format(key, cx)
-    search = "q={}".format("courses")
+    search = "q={}".format("english%20online%20course")
     fmt = "filter=1&start={}&num=10&alt=json".format(start)
-    url = "{}?{}?{}?{}".format(
+    url = "{}?{}&{}&{}".format(
         url,
         q,
         search,
         fmt
     )
     values = requests.get(url)
-    import ipdb
-    ipdb.set_trace()
+    items = values.json().get('items')
+    for item in items:
+        print "save"
+        CoursesModel().save(
+            item.get('title'),
+            item.get('formattedUrl')
+        )
