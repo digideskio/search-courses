c494c07b094bee03b52b994a0515e2072ae72046
Denis Aoki <dnsaoki@gmail.com>
2016-11-23 14:58:57 -0200

    sincronizando banco de dados

--- search/views.py
+++ search/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
-
+from search.tasks import TaskSearchCourses
 
 class IndexView(TemplateView):
     template_name = 'search/index.html'
+
+    def get_context_data(self, **kwargs):
+        TaskSearchCourses()
