8b85514247e3cc0a2d5cac658e932a330c597e0a
Denis Aoki <dnsaoki@gmail.com>
2016-11-24 18:12:39 -0200

    Mostrando na tela todos os cursos registrados

--- search/views.py
+++ search/views.py
@@ -1,13 +1,29 @@
+import re
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
 # Create your views here.
 from search.tasks import TaskSearchCourses
+from search.dbase import DataBase
 
 class IndexView(TemplateView):
     template_name = 'search/index.html'
 
     def get_context_data(self, **kwargs):
-        return {}
+        r = "^http[s]?"
+        iter = DataBase().get_all()
+        context = []
+        for i in iter:
+            address = i[1]
+            if re.match(r, i[1]) is None:
+                address = "http://%s" % i[1]
+            context.append(
+                {
+                    "name": i[0],
+                    "address": address
+                }
+            )
+        print context
+        return {'context': context}
 
 
 class TaskCallView(View):
