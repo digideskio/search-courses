c8579507508f40443d6d20673e71461d7d165894
Denis Aoki <dnsaoki@gmail.com>
2016-12-04 20:10:27 -0200

    Add varias querys

--- search/tasks.py
+++ search/tasks.py
@@ -5,36 +5,40 @@ from background_task import background
 from search.dbase import DataBase
 from search import settings
 
-@background(schedule=60*20)
+#  @background(schedule=60*20)
 def TaskSearchCourses():
+    querys = ["english%20online%20course", "curso%20ingles%20online", "curso%20online%20interativo"]
     key = settings.KEY
     cx = settings.CX
     url_base = "https://www.googleapis.com/customsearch/v1"
     q = "key={}&cx={}".format(key, cx)
-    search = "q={}".format("english%20online%20course")
-    for start in range(0, 5):
-        print "search page: %s" % start
-        logging.debug("search page: %s" % start)
-        fmt = "filter=1&start={}&num=10&alt=json".format(start*10 + 1)
-        url = "{}?{}&{}&{}".format(
-            url_base,
-            q,
-            search,
-            fmt
-        )
-        response = requests.get(url)
-        if response.status_code == 200:
-            items = response.json().get('items')
-            DataBase().save(items)
-            logging.debug("page done: %s" % start)
-            print "page done: %s" % start
-        else:
-            logging.debug("Error in page: {}, status: {}".format(
-                start,
-                response.status_code)
+    for query in querys:
+        print "search for query: %s" % query
+        search = "q={}".format(query)
+        for start in range(0, 5):
+            print "search page: %s" % start
+            logging.debug("search page: %s" % start)
+            fmt = "filter=1&start={}&num=10&alt=json".format(start*10 + 1)
+            url = "{}?{}&{}&{}".format(
+                url_base,
+                q,
+                search,
+                fmt
             )
-            print "Error in page: {}, status: {}".format(
-                start,
-                response.status_code
-            )
-    TaskSearchCourses()
+            print "URL: %s" % url
+            response = requests.get(url)
+            if response.status_code == 200:
+                items = response.json().get('items')
+                DataBase().save(items)
+                logging.debug("page done: %s" % start)
+                print "page done: %s" % start
+            else:
+                logging.debug("Error in page: {}, status: {}".format(
+                    start,
+                    response.status_code)
+                )
+                print "Error in page: {}, status: {}".format(
+                    start,
+                    response.status_code
+                )
+    #  TaskSearchCourses()
